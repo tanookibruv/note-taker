b4b251d2c4b5afaa974ebb13cd47896c9feadbda
tanookibruv <erendon1836@gmail.com>
2021-10-08 21:15:21 -0700

    succeeded in get request for lading page and note page

--- Develop/server.js
+++ Develop/server.js
@@ -7,4 +7,13 @@ const PORT = process.env.PORT || 3001
 const app = express();
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+
 app.listen(PORT, () => console.log ('Sever is listening to port 3001'));
