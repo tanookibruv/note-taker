3ea64f4b32970e7f3e5e90ec23b8ec8df0e3302f
tanookibruv <erendon1836@gmail.com>
2021-10-08 20:59:10 -0700

    made server.js file to start server up. need GET and POST request

---
+++ Develop/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001
+const app = express();
+
+
+app.listen(PORT, () => console.log ('Sever is listening to port 3001'));
